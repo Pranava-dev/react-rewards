cccd380b782719c6f1d7223cd017fdf7e0a7cb82
Pranava-dev <pranava259@gmail.com>
2025-11-28 11:19:13 +0530

    loadcolorupdate

---
+++ src/calculateRewards.test.js
@@ -0,0 +1,7 @@
+/*import { calculateRewardPoints } from "./utils/calculateRewards";
+
+test("calculate reward points correctly",()=>{
+   expect(calculateRewardPoints(120)).toBe(90);
+   expect(calculateRewardPoints(75)).toBe(25);
+   expect(calculateRewardPoints(50)).toBe(0);
+});*/
